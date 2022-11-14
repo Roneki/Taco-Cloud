05df572ec9d8c7bce8c14a461e198fb68bf142b2
roneki <roneki.manamperi@itechro.com>
2022-11-14 12:00:10 +0530

    Initial Commit

---
+++ src/main/java/tacos/HomeController.java
@@ -0,0 +1,15 @@
+package tacos;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+}
